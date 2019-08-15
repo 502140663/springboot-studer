303ed8f47edb2b9b25eac9739f9ed87dea4a2354
huangfuchao <huangfuchao@bluemoon.com.cn>
2019-08-15 17:53:21 +0800

    1

---
+++ web-exception-resp/src/main/java/cn/com/bluemoon/webexceptionresp/WebExceptionRespApplication.java
@@ -0,0 +1,13 @@
+package cn.com.bluemoon.webexceptionresp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExceptionRespApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebExceptionRespApplication.class, args);
+    }
+
+}
