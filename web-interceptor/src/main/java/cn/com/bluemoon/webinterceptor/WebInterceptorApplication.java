3cde29cc631b51f02b17e07f360bc4319d7e65c0
huangfuchao <huangfuchao@bluemoon.com.cn>
2019-08-17 16:22:11 +0800

    一些配资文件

---
+++ web-interceptor/src/main/java/cn/com/bluemoon/webinterceptor/WebInterceptorApplication.java
@@ -0,0 +1,13 @@
+package cn.com.bluemoon.webinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInterceptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebInterceptorApplication.class, args);
+    }
+
+}
